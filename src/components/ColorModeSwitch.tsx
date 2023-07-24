57487e011b65bbf8a4f3309edb8f2a07a276d7e5
Arjun Sharma <arjunsharma5330@gmail.com>
2023-07-24 10:55:09 +0530

    Build search input and changed padding in gamegrid

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
           isChecked={colorMode === "dark"}
           onChange={toggleColorMode}
         />
-        <Text>{colorMode} mode</Text>
+        <Text whiteSpace="nowrap">{colorMode} mode</Text>
       </HStack>
     </>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding={10}
+        padding={5}
         spacing={3}
       >
         {isLoading &&
