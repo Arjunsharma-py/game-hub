f0b2e400081fe61bef303605b1ba69bae299a7a2
Arjun Sharma <arjunsharma5330@gmail.com>
2023-07-24 10:25:05 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noplaceholder from "../assets/placeholder-image.jpg";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noplaceholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
